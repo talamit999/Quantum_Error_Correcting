4ae65a73825bf3aa3b9c58d294c98916809e7126
talamit999 <102660057+talamit999@users.noreply.github.com>
2025-01-07 13:57:14 +0200

    Fix One Error
    Add a functionality to correct up to one error in 3-qubit-code

--- three_qubits_rep_code.py
+++ three_qubits_rep_code.py
@@ -1,13 +1,15 @@
 import numpy as np
 from squanch import *
+import qutip as qt
+
 
 _0 = np.array([1, 0], dtype=np.complex64)
 _1 = np.array([0, 1], dtype=np.complex64)
-_M0 = np.outer(_0, _0)
+_M0 = np.outer(_0, _0) #
 _M1 = np.outer(_1, _1)
 
-a = 0.6  # Amplitude for |0> in qubit0
-b = 0.8  # Amplitude for |1> in qubit0
+a = 0.2  # Amplitude for |0> in qubit0
+b = np.sqrt(1- a**2)  # Amplitude for |1> in qubit0
 _psi = np.array([a, b], dtype=np.complex64)
 _Mpsi = np.outer(_psi, _psi)
 
@@ -24,6 +26,7 @@ _X = np.array([[0, 1],
                [1, 0]])
 
 _X1_I2_I3 = np.kron(np.kron(np.kron(np.kron(_X, _I), _I), _I), _I)
+_I1_X2_I3 = np.kron(np.kron(np.kron(np.kron(_I, _X), _I), _I), _I)
 _X1_X2_I3 = np.kron(np.kron(np.kron(np.kron(_X, _X), _I), _I), _I)
 
 
@@ -47,8 +50,7 @@ def E(qsystem, operator=None):
 def three_qubits_rep_code(operator):
 
     state = np.kron(np.kron(np.kron(np.kron(_Mpsi, _M0), _M0), _M0), _M0)
-    num_qubits = 5
-    qsystem = QSystem(num_qubits, state=state)
+    qsystem = QSystem(num_qubits=5, state=state)
     qubit0 = Qubit(qsystem, 0)
     qubit1 = Qubit(qsystem, 1)
     qubit2 = Qubit(qsystem, 2)
@@ -57,19 +59,21 @@ def three_qubits_rep_code(operator):
 
     #print(f"Initial quantum state (|psi>):\n", qsystem.state)
 
+    #encoding
     CNOT(qubit0, qubit1)
-    #print("Quantum state after applying CNOT gate:\n", qsystem.state)
-
     CNOT(qubit1, qubit2)
+
+
     #print("Quantum state after applying CNOT gate:\n", qsystem.state)
 
-
+    #error
     E(qsystem, operator)
 
     H(a1)
     H(a2)
 
-    # print("Quantum state:\n", syndrome_sys.state)
+
+    #decoding
     CU(a1, qubit0, _Z)
     CU(a1, qubit1, _Z)
 
@@ -78,18 +82,50 @@ def three_qubits_rep_code(operator):
 
     H(a1)
     H(a2)
+    #print("Quantum state:\n", qsystem.state)
 
     measurement_result = a1.measure()
-    #print(f"Measured qubit a1: {measurement_result}")
+    print(f"Measured qubit a1: {measurement_result}")
     measurement_result2 = a2.measure()
-    #print(f"Measured qubit a2: {measurement_result2}")
-    return measurement_result, measurement_result2
+    print(f"Measured qubit a2: {measurement_result2}")
+    return qsystem, measurement_result, measurement_result2
 
+def fix_error(qsystem):
+    '''
+    can only fix one qubit flip
+    :param qsystem: current Qsystem after measuring eunsiles
+    :return: original system
+    '''
+    CNOT(qsystem.qubit(0), qsystem.qubit(1))
+    CNOT(qsystem.qubit(0), qsystem.qubit(2))
+    TOFFOLI(qsystem.qubit(1), qsystem.qubit(2), qsystem.qubit(0))
+    qsystem.measure_qubit(1)
+    qsystem.measure_qubit(2)
+
+    # Iterate through rows and columns to print the diagonal of the matrix
+    #print("Quantum state now:")
+    #for i in range(len(qsystem.state)):
+    #    print(f"{qsystem.state[i][i]:.4f}")
+    measure0 = gates.expand(_M0, 0, qsystem.num_qubits, "0" + str(0) + str(qsystem.num_qubits))
+    prob0 = np.trace(np.dot(measure0, qsystem.state))
+    print(f"a is {np.sqrt(np.abs(prob0)):.2f}")
 
 
 if __name__ == "__main__":
-    for i in range(100):
-        print(i)
-        a1, a2 = three_qubits_rep_code(_X1_X2_I3)
-        if a1!=0 or a2!=1:
-            print("Error!")
+    #for i in range(100):
+        #print(i)
+        #a1, a2 = three_qubits_rep_code(_X1_X2_I3)
+        #if a1!=0 or a2!=1:
+        #    print("Error!")
+    '''
+    qsystem = QSystem(num_qubits=3)
+    qubit0 = Qubit(qsystem, 0)
+    qubit1 = Qubit(qsystem, 1)
+    qubit1 = Qubit(qsystem, 2)
+    index = 0
+    measure0 = gates.expand(_M0, index, qsystem.num_qubits, "0" + str(index) + str(qsystem.num_qubits))
+    print("measure0:")
+    print(measure0)
+    '''
+    qsys, _, _ = three_qubits_rep_code(_I1_X2_I3)
+    fix_error(qsys)
